130cabdf537fcbedf9f65e39035e80a4a36ba9dd
Lvalenlu <102125441+Lvalenlu@users.noreply.github.com>
2023-08-31 10:01:41 -0500

    Update colas.java

--- colas.java
+++ colas.java
@@ -1,327 +1,106 @@
 import java.util.Scanner;
-
-import java.util.Random;
-
- 
-
-class FilaUsuario
-
-{
-
- 
-
-  private int[] fila;
-
-  private int index;
-
-  private int nUsuarios;
-
- 
-
-  public FilaUsuario ()
-
-  {
-
-    this.fila = new int[20];
-
-      this.index = 0;
-
-      this.nUsuarios = 0;
-
-  }
-
- 
-
-  public FilaUsuario (int n)
-
-  {
-
-    this.fila = new int[n];
-
-    this.index = 0;
-
-    this.nUsuarios = 0;
-
-  }
-
- 
-
-  public void llenarFila ()
-
-  {
-
-    if (fila[fila.length - 1] == 0)
-
-      {
-
-    if (index < fila.length)
-
-      {
-
-        fila[index] = ++nUsuarios;
-
-        index++;
-
-      }
-
-      }
-
-    else
-
-      {
-
-    System.out.println ("La fila esta llena");
-
-      }
-
-    imprimirFila ();
-
-  }
-
- 
-
-public void anexarUsuariosAleatorios(){
-
-        Random random = new Random();
-
-        int numUsuarios = random.nextInt(20) + 1; // De 1 a 5 usuarios
-
-        for (int i = 0; i < numUsuarios; i++) {
-
-            if (index < fila.length) {
-
-                fila[index] = ++nUsuarios;
-
-                index++;
-
-            } else {
-
-                System.out.println("La fila está llena");
-
-                break;
-
-            }
-
+public class Main {
+    private Pila unaPila;
+    private Scanner sc;
+    public Main (int n){
+        if (n > 0){
+            this.unaPila = new Pila(n);
+            this.sc = new Scanner (System.in);
         }
-
-        imprimirFila();
-
     }
-
- 
-
-  public void retirarUsuarioFila ()
-
-  {
-
-    if (index != 0)
-
-      {
-
-    for (int i = 0; i < fila.length - 1; i++)
-
-      {
-
-        fila[i] = fila[i + 1];
-
-      }
-
-    fila[fila.length - 1] = 0;
-
-    index--;
-
-    imprimirFila ();
-
-      }
-
-    else
-
-      {
-
-    System.out.println ("La fila esta vacia");
-
-      }
-
-  }
-
- 
-
- 
-
-  public boolean estaVacia ()
-
-  {
-
-    return fila[0] == 0;
-
-  }
-
- 
-
- 
-
-  public void imprimirFila ()
-
-  {
-
-    System.out.println ();
-
-    for (int i = 0; i < fila.length; i++)
-
-      {
-
-    System.out.print (fila[i] + " ");
-
-      }
-
-    System.out.println ();
-
-  }
-
+    public void procesarOperacionesPila(){
+        String menu = "1. Agregar dato pila. \n2. Imprimir pila \n" + "3. Retirar dato pila \n4. Estado de la pila\n5.Salir" + "\n\nSeleccione opcion";
+        int opc = 1;
+        do{
+            System.out.println(menu);
+            opc = sc.nextInt();
+            switch(opc){
+                case 1:
+                    unaPila.llenarPila();
+                    break;
+                case 2:
+                    unaPila.imprimirPila();
+                    break;
+                case 3:
+                    unaPila.retirarUsuarioPila();
+                    break;
+                case 4:
+                    System.out.println(unaPila.consultarEstadoPila() + "\n");
+                    break;
+                default:
+                    System.out.println("Opción errada");
+                    break;
+            }
+        }while(opc != 5);
+    }
+    public static void main(String []args){
+        int usuario = 5;
+        Main app = new Main(usuario);
+        app.procesarOperacionesPila();
+    }
 }
-
- 
-
- 
-
-public class Main
-
-{
-
- 
-
- 
-
-  private FilaUsuario unaFila;
-
-  private Scanner sc;
-
- 
-
- 
-
-  public Main (int n)
-
-  {
-
-    if (n > 0)
-
-      {
-
-    this.unaFila = new FilaUsuario (n);
-
-    this.sc = new Scanner (System.in);
-
-      }
-
-  }
-
- 
-
- 
-
-  public void procesarOperacionesFila ()
-
-  {
-
-    String menu = "1. Agregar usuario fila.\n2. Agregar usuario aleatorio. \n"
-
-      + "3. Imprimir fila\n4. Retirar usuario fila\n"
-
-      + "5. Estado de la fila\n6. Salir\n\n Seleccione opcion"; 
-
-     int opc = 1;
-
-    do
-
-      {
-
-    System.out.println (menu);
-
-    opc = sc.nextInt ();
-
-    switch (opc)
-
-      {
-
-      case 1:
-
-        unaFila.llenarFila ();
-
-        break;
-
-      case 2:
-
-        unaFila.anexarUsuariosAleatorios();
-
-        break;
-
-      case 3:
-
-        unaFila.imprimirFila ();
-
-        break;
-
-      case 4:
-
-        unaFila.retirarUsuarioFila ();
-
-        break;
-
-      case 5:
-
-        verificarEstado ();
-
-        break;
-
-      }
-
-      }
-
-    while (opc != 6);
-
-  }
-
- 
-
- 
-
-  private void verificarEstado ()
-
-  {
-
-    if (unaFila.estaVacia ())
-
-      {
-
-    System.out.println ("La fila esta vacia");
-
-      }
-
-    else
-
-      {
-
-    System.out.println ("En la fila hay usuarios");
-
-      }
-
-  }
-
- 
-
- 
-
-  public static void main (String[]args)
-
-  {
-
-    int usuarios = 20;
-
-    Main app = new Main (usuarios);
-
-    app.procesarOperacionesFila ();
-
-  }
-
+class Pila{
+    private int [] pila;
+    private int index;
+    private int nProceso;
+    /*Se crea un arreglo*/
+    public Pila(){
+        this.pila = new int [20];
+        this.index = pila.length - 1;
+        this.nProceso = 0;
+    }
+    public Pila(int n){
+        this.pila = new int [n];
+        this.index = pila.length -1;
+        this.nProceso = 0;
+    }
+    /*metodo que anexa datos a la pila*/
+    public void llenarPila(){
+        if (index > pila.length){
+            index = pila.length - 1;
+        }
+        if (index >= 0){
+            pila[index] = ++nProceso;
+            index--;
+        }else{
+            int i = index;
+            while (pila[i] != 0) {
+                pila [i-1] = pila[i];
+                i++;
+            }
+            pila [i-1] = ++nProceso;
+            index--;
+        }
+        imprimirPila();
+    }
+    public void retirarUsuarioPila(){
+        boolean estado = true;
+        int i = 0;
+        while (estado && i < pila.length){
+            if (pila[i] != 0){
+                estado = false;
+                --nProceso;
+                ++index;
+            }
+            i++;
+        }
+    }
+    public String consultarEstadoPila(){
+        String estado = "";
+        if (pila[0] != 0){
+            estado = "La pila esta esta llena";
+        }else if (pila[pila.length - 1] == 0){
+            estado = "La pila esta esta vacia";
+        }else if (nProceso != 0){
+            estado = "La pila tiene datos";
+        }
+        return estado;
+    }
+    public void imprimirPila(){
+        System.out.println("");
+        for(int i = 0; i < pila.length; i++){
+            System.out.println(pila[i]+"");
+        };
+        System.out.println("");    
+    }
 }
